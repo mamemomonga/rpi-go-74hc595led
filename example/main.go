08bee37b562cb8401619e73355b0e73bb1e7327a
mamemomonga <mamemomonga@gmail.com>
2019-08-11 01:48:06 +0000

    bcm2835.SpiTransfernはポインタ先を破壊するので修正

--- example/main.go
+++ example/main.go
@@ -12,9 +12,23 @@ func main() {
 	log.Printf("Start")
 
 	for {
+		for i:=0; i<2; i++ {
+			leds.AllHigh()
+			time.Sleep(time.Millisecond * 100)
+			leds.AllLow()
+			time.Sleep(time.Millisecond * 100)
+		}
 		for i := uint8(0); i < 8; i++ {
 			leds.Set(0, i, 1)
-			time.Sleep(time.Millisecond * 500)
+			time.Sleep(time.Millisecond * 100)
+		}
+		for i := uint8(0); i < 8; i++ {
+			leds.Set(0, 8-i, 0)
+			time.Sleep(time.Millisecond * 100)
+		}
+		for i := uint8(0); i < 8; i++ {
+			leds.Set(0, i, 1)
+			time.Sleep(time.Millisecond * 50)
 			leds.Set(0, i, 0)
 		}
 	}
--- simple/simple.go
+++ simple/simple.go
@@ -14,11 +14,26 @@ func Start(chip_n uint8) {
 	pi595led.Init()
 	chip_num = chip_n
 	leds = make([]uint8,chip_num)
+	AllLow()
 }
 
 func Finalize() {
 	bcm2835.SpiEnd()
 	bcm2835.Close()
+}
+
+func AllHigh() {
+	for i:=uint8(0); i<chip_num; i++ {
+		leds[i]=255
+	}
+	bcm2835.SpiTransfern(append([]uint8{},leds...))
+}
+
+func AllLow() {
+	for i:=uint8(0); i<chip_num; i++ {
+		leds[i]=0
+	}
+	bcm2835.SpiTransfern(append([]uint8{},leds...))
 }
 
 func Set(chip uint8, pin uint8, state uint8) {
@@ -28,6 +43,11 @@ func Set(chip uint8, pin uint8, state uint8) {
 	case 1:
 		leds[chip] |=  ( 1 << pin )
 	}
-	bcm2835.SpiTransfern(leds)
+	bcm2835.SpiTransfern(append([]uint8{},leds...))
 }
 
+func reverse8Bit(b byte) (r byte) {
+	b = ((b & 0x55) << 1) | ((b & 0xAA) >> 1)
+	b = ((b & 0x33) << 2) | ((b & 0xCC) >> 2)
+	return (b << 4) | (b >> 4)
+}
